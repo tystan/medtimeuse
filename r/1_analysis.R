3181baaba0b4e95a925b3237e39d460a1415ab3e
Ty Stanford <tystan@gmail.com>
2023-03-02 21:34:03 +1030

    More supp material

--- r/1_analysis.R
+++ r/1_analysis.R
@@ -26,7 +26,6 @@ suppressPackageStartupMessages({
 
 # ---- funcs_and_consts ----
 
-
 add_alpha <- function(col, alpha = 1) {
   apply(
     sapply(col, col2rgb) / 255, 2,
@@ -34,17 +33,13 @@ add_alpha <- function(col, alpha = 1) {
   )
 }
 
-
 stage_ins_col <- add_alpha(c("cyan", "magenta"), 0.25)
 stage_out_col <-  add_alpha(c("cyan", "magenta"), 0.75)
 names(stage_ins_col) <- names(stage_out_col) <- NULL
 
-
 med_ins_col <- add_alpha(c("orange", "purple"), 0.25)
 med_out_col <-  add_alpha(c("orange", "purple"), 0.75)
 names(med_ins_col) <- names(med_out_col) <- NULL
-
-
 
 pal_use <- "Plasma" # "Temps", "Zissou 1"
 plas_pal <- hcl.colors(n = 10, palette = pal_use, rev = FALSE)
@@ -52,13 +47,18 @@ sed_ins_col <- add_alpha(plas_pal, 0.25)
 sed_out_col <-  add_alpha(plas_pal, 0.75)
 names(sed_ins_col) <- names(sed_out_col) <- NULL
 
-
-
 pal_use <- "Viridis" 
 vir_pal <- hcl.colors(n = 11, palette = pal_use, rev = FALSE)
 ach_ins_col <- add_alpha(vir_pal, 0.25)
 ach_out_col <-  add_alpha(vir_pal, 0.75)
 names(ach_ins_col) <- names(ach_out_col) <- NULL
+
+pal_use <- "Classic Tableau" 
+ct_pal <- palette.colors(n = 4, palette = pal_use)
+timeuse_col <-  add_alpha(ct_pal, 0.75)
+names(timeuse_col) <- NULL
+
+
 
 
 # ---- read ----
@@ -88,6 +88,44 @@ colnames(tu_ilrs) <- paste0("ilr", 1:D)
 sedach_dat <- bind_cols(sedach_dat, tu_ilrs)
 colnames(sedach_dat)
 
+
+# ---- exploratory_0 ----
+
+join_dat <-
+  sedach_dat %>% 
+    select(StudyID, TrialStage, sed_score, ach_score) 
+
+join_dat <-
+  inner_join(
+    join_dat %>% dplyr::filter(TrialStage == "Baseline"),
+    join_dat %>% dplyr::filter(TrialStage == "12 Months"),
+    "StudyID"
+  ) %>%
+    mutate(
+      sed_ot = 
+        case_when(
+          sed_score.x < sed_score.y ~ "(c) sed increase",
+          sed_score.x == sed_score.y ~ "(b) sed constant",
+          sed_score.x > sed_score.y ~ "(a) sed decrease",
+          TRUE ~ as.character(NA)
+        ),
+      ach_ot = 
+        case_when(
+          ach_score.x < ach_score.y ~ "(c) ach increase",
+          ach_score.x == ach_score.y ~ "(b) ach constant",
+          ach_score.x > ach_score.y ~ "(a) ach decrease",
+          TRUE ~ as.character(NA)
+        )
+    )
+
+incdec_tab <- 
+  with(join_dat, table(sed_ot, ach_ot, useNA = "ifany")) 
+
+incdec_tab %>%
+  kable(.)
+
+# sum(incdec_tab)
+# sum(diag(incdec_tab)) / sum(incdec_tab)
 
 # ---- exploratory_1 ----
 
@@ -149,9 +187,9 @@ sedach_dat %>%
   mutate(
     med = 
       case_when(
-        med == "ach_score"  ~ "Anticholinergic",
+        med == "ach_score" ~ "Anticholinergic",
         med == "sed_score" ~ "Sedative",
-        TRUE                 ~ "ERROR!!!"
+        TRUE               ~ "ERROR!!!"
       )
   ) %>%
   na.omit(.) %>%
@@ -247,7 +285,7 @@ d_ilr_long <-
   dplyr::select(-starts_with("tu_")) %>%
   pivot_longer(
     ., 
-    cols = ilr1:ilr3, 
+    cols = starts_with("ilr"), 
     names_to = "ilr_no", 
     values_to = "val"
   )
@@ -300,10 +338,10 @@ d_ilr_long %>%
 
 dat_lng <- 
   sedach_dat %>%
-  dplyr::select(-starts_with("tu_")) %>%
+  dplyr::select(-starts_with("tu_")) %>% # keep only ilrs not time-use vars
   pivot_longer(
     ., 
-    cols = ilr1:ilr3, 
+    cols = starts_with("ilr"), 
     names_to = "ilr.no", 
     values_to = "val"
   )
@@ -379,8 +417,152 @@ check_model(
 
 
 
+
+# ---- pred_const_over_time_setup ----
+
+
+
+get_mod_pred <- function(mod, dat) {
+  pred_val <- predict(mod, newdata = dat, re.form = NA)
+  pred_newd <- cbind.data.frame(pred_val, dat)
+  pred_newd_w <- 
+    pivot_wider(pred_newd, names_from = "ilr.no", values_from = "pred_val")
+  ilr_cols <- grepl("ilr", colnames(pred_newd_w))
+  time_use <- 1440 * unclass(ilrInv(pred_newd_w[, ilr_cols]))
+  colnames(time_use) <- c("sl", "sed", "lp", "mv")
+  return(bind_cols(pred_newd_w, time_use))
+}
+
+new_sed <- expand.grid(
+  ilr.no = c("ilr1", "ilr2", "ilr3"),
+  TrialStage = c("Baseline", "12 Months"),
+  sed_score = seq(0, 9, 1)
+)
+
+new_ach <- expand_grid(
+  ilr.no = c("ilr1", "ilr2", "ilr3"),
+  TrialStage = c("Baseline", "12 Months"),
+  ach_score = seq(0, 11, 1)
+)
+
+preds <- predict(mod_sed, newdata = new_sed, re.form = NA)
+pred_df <- cbind.data.frame(preds, new_sed)
+
+sed_preds <-
+  get_mod_pred(mod_sed, new_sed) %>% 
+  rename(medload = sed_score) %>%
+  mutate(med = "Sedative load")
+
+sed_preds %>%
+  arrange(desc(TrialStage), medload) %>%
+  select(TrialStage, med, medload, everything(), -starts_with("ilr")) %>%
+  kable(., digits = 0)
+
+
+(sed_preds %>%
+  dplyr::filter(TrialStage == "12 Months", medload == 4) %>%
+  select(6:9)) -
+(sed_preds %>%
+  dplyr::filter(TrialStage == "Baseline", medload == 2) %>%
+  select(6:9))
+
+preds <- predict(mod_ach, newdata = new_ach, re.form = NA)
+pred_df <- cbind.data.frame(preds, new_ach)
+
+
+ach_preds <-
+  get_mod_pred(mod_ach, new_ach) %>% 
+  rename(medload = ach_score) %>%
+  mutate(med = "Anticholinergic load")
+
+ach_preds %>%
+  arrange(desc(TrialStage), medload) %>%
+  select(TrialStage, med, medload, everything(), -starts_with("ilr")) %>%
+  kable(., digits = 0)
+
+
+all_pred <- 
+  bind_rows(sed_preds, ach_preds) %>%
+  select(-starts_with("ilr"))
+
+
+all_pred <-
+  inner_join(
+    all_pred %>% filter(TrialStage == "12 Months"),
+    all_pred %>% filter(TrialStage == "Baseline"),
+    c("med", "medload")
+  )
+
+all_pred <-
+  all_pred %>%
+  mutate(
+    change_Sleep = sl.x - sl.y,
+    change_Sedentary = sed.x - sed.y,
+    change_LightPA = lp.x - lp.y,
+    change_MVPA = mv.x - mv.y
+  )
+
+all_pred <-
+  all_pred %>%
+  select(-matches("\\.(x|y)", perl = TRUE))
+
+
+# ---- specific_preds ----
+
+(all_pred %>%
+  dplyr::filter(med == "Sedative load", medload == 4) %>%
+  select(3:6)) -
+(all_pred %>%
+  dplyr::filter(med == "Sedative load", medload == 2) %>%
+  select(3:6))
+###  change_Sleep change_Sedentary change_LightPA change_MVPA
+###     -3.821428         14.51255      -4.866429   -5.824697
+
+(all_pred %>%
+    dplyr::filter(med != "Sedative load", medload == 4) %>%
+    select(3:6))
+### change_Sleep change_Sedentary change_LightPA change_MVPA
+###        -1.92             28.3          -15.6       -10.8
+
+# ---- pred_const_over_time ----
+
+all_pred_lng <-
+  all_pred %>%
+  pivot_longer(
+    ., cols = starts_with("change"), names_to = "timeuse", values_to = "time"
+  ) %>%
+  mutate(
+    timeuse =  gsub("change_", "", timeuse),
+    med = fct_inorder(med),
+    timeuse = fct_inorder(timeuse)
+  )
+
+all_pred_lng %>%
+  ggplot(., aes(x = medload, y = time, group = timeuse)) +
+  geom_hline(yintercept = 0, lty = 2) +
+  geom_line(aes(colour = timeuse)) +
+  geom_point(aes(colour = timeuse)) +
+  facet_wrap( ~ med) +
+  scale_colour_manual(values = timeuse_col) +
+  scale_x_continuous(breaks = c(0, 2, 4, 6, 8, 10)) +
+  theme_bw() +
+  labs(
+    x = "Medication Load at 12-months",
+    y = "Change in Activity (min/d)",
+    colour = "Activity"
+  ) +
+  theme(text = element_text(family = "serif"))
+
+
 # ---- pred_df ----
 
+
+
+get_pred_diff <- function(mod, dat) {
+  time_use <- get_mod_pred(mod, dat)
+  time_use <- time_use[, c("sl", "sed", "lp", "mv")]
+  return(time_use[2, ] - time_use[1, ])
+}
 
 newd1 <- 
   expand.grid(
@@ -415,46 +597,21 @@ newd_ach <-
   rename(ach_score = score)
 
 
-
-get_sed_diff <- function(.) {
-  pred_val <- predict(., newdata = newd_sed, re.form = NA)
-  pred_newd <- cbind.data.frame(pred_val, newd_sed)
-  pred_newd_w <- spread(pred_newd, key = ilr.no, value = pred_val)
-  ilr_cols <- grepl("ilr", colnames(pred_newd_w))
-  time_use <- 1440 * unclass(ilrInv(pred_newd_w[, ilr_cols]))
-  colnames(time_use) <- c("sl", "sed", "lp", "mv")
-  # return(time_use[2, "sed"] - time_use[1, "sed"])
-  return(time_use[2, ] - time_use[1, ])
-}
-
 cat(
   "This is expected change in minutes to the time-use composition\n",
   "when going from sed load = 2 to sed load = 4 from baseline to 12 months.\n"
 )
-get_sed_diff(mod_sed) %>% 
-  tibble(time_use_cat = names(.), minutes = .) %>%
+get_pred_diff(mod_sed, newd_sed) %>% 
   kable(., digits = 1)
 
-
-
-get_ach_diff <- function(.) {
-  pred_val <- predict(., newdata = newd_ach, re.form = NA)
-  pred_newd <- cbind.data.frame(pred_val, newd_ach)
-  pred_newd_w <- spread(pred_newd, key = ilr.no, value = pred_val)
-  ilr_cols <- grepl("ilr", colnames(pred_newd_w))
-  time_use <- 1440 * unclass(ilrInv(pred_newd_w[, ilr_cols]))
-  colnames(time_use) <- c("sl", "sed", "lp", "mv")
-  # return(time_use[2, "sed"] - time_use[1, "sed"])
-  return(time_use[2, ] - time_use[1, ])
-}
 
 cat(
   "This is expected change in minutes to the time-use composition\n",
   "when going from anticholinergic load = 2 to sed load = 4\n",
   "from baseline to 12 months.\n"
 )
-get_ach_diff(mod_ach) %>% 
-  tibble(time_use_cat = names(.), minutes = .) %>%
+# somewhat of an extrapolation
+get_pred_diff(mod_ach, newd_ach) %>% 
   kable(., digits = 1)
 
 
